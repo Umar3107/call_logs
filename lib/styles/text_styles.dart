e2e1458bfcb4d0f36dfddccad6d9f303bebefd98
Umar3107 <marslord31072@gmail.com>
2022-05-11 16:32:59 +0300

    les 81 -82
    не выходит изображение телефона при запуске(заленоватый еще такой)

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyle{
+  static TextStyle bold17({Color? color = AppColors.primary}) {
+    return TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.bold,
+        color: color,
+    );
+  }
+
+  static TextStyle regular15({Color? color = AppColors.secondary}) {
+    return TextStyle(
+      fontSize: 15,
+      color: color,
+    );
+  }
+}
